6bf7c34a6c797d303a7edf564fe065a9b3dce422
agayushh <agayush088@gmail.com>
2025-07-28 19:39:57 +0530

    initialize Express application with CORS and JSON body parsing

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use("/api/v1", router);
+
+export default app;
