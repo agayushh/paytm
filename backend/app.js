f02439f467f767c87244e001bda3a6f9e824962e
agayushh <agayush088@gmail.com>
2025-08-01 20:16:23 +0530

    fix: update router import path to include index.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { router } from "./routes";
+import { router } from "./routes/index.js";
 
 const app = express();
 
@@ -15,5 +15,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use("/api/v1", router);
+
 
 export default app;
