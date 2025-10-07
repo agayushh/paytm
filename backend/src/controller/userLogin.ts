ec55c55878c90305220445a8540d50fc813d0e2c
agayushh <agayush088@gmail.com>
2025-10-07 23:52:56 +0530

    feat: remove bankName field from User model and update registration logic

--- backend/src/controller/userLogin.ts
+++ backend/src/controller/userLogin.ts
@@ -1,6 +1,57 @@
 import prisma from "../db";
 import bcrypt from "bcrypt";
 
+export async function registerUser({
+  username,
+  email,
+  password,
+  accountNumber,
+}: {
+  username: string;
+  email: string;
+  password: string;
+  accountNumber: number;
+}) {
+  try {
+    if (
+      !username?.trim() ||
+      !email?.trim() ||
+      !password?.trim() ||
+      !accountNumber
+    ) {
+      return { status: "error", message: "All fields are required" };
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ username }, { email }, { accountNum: accountNumber }],
+      },
+    });
+    if (existingUser) {
+      return { status: "error", message: "User already exists" };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+        accountNum: accountNumber,
+      },
+    });
+
+    return {
+      status: "success",
+      message: "User registered successfully",
+      data: newUser,
+    };
+  } catch (error) {
+    return { status: "error", message: "Something went wrong" };
+  }
+}
+
 export async function userLogin({
   username,
   password,
